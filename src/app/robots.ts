6c5a7709aeb15b0f32da4299ffd688e83b486cd7
koichi0903 <koichi0903.17@gmail.com>
2026-08-12 20:53:08 +0900

    chore: remove unused public test page

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,7 +9,6 @@ export default function robots(): MetadataRoute.Robots {
       disallow: [
         "/admin",
         "/api/",
-        "/test",
       ],
     },
     sitemap: `${SITE_URL}/sitemap.xml`,
